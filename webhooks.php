78af8604d9f4417ffbd013e00843777157b16f80
satangsir <kerkkiet@gmail.com>
2018-07-30 21:55:45 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'WrZa2jvsQA6EfbMr3SrqOfRxjDiXjOsAAe3uGtIZyKBQxC4kkmR3PD2Y5274xdLdjEAIoIY+QXNQYFhZMJ7XP5TVBcBq1BOqMSZ84zWvOWQg59zmeT8ngaklgClLDUd0rUY6Uevwyd+ZDrR82bcYBQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
